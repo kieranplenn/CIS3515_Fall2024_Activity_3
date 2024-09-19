8e22a8af2a6afd481ae7eb799aab6f41bc05298a
kieranplenn <tuk25850@temple.edu>
2024-09-19 12:20:07 -0400

    First commit functions: getCount(), getItem(), getItemId().

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,22 +1,17 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(private val context: Context, private val numbers: Array<Int>) : BaseAdapter() {
 
-    override fun getCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getCount() = numbers.size
 
-    override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
-    }
+    override fun getItem(position: Int) = numbers[position]
 
-    override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
-    }
+    override fun getItemId(position: Int) = numbers[position].toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         TODO("Not yet implemented")
