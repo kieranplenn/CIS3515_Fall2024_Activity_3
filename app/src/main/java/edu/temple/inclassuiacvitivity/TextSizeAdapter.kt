c55aed4df8c2c0141b8dd259acdca3a30a2a5b77
kieranplenn <tuk25850@temple.edu>
2024-09-22 18:24:00 -0400

    Finished simple implementation of TextSizeAdapter and used it to set spinner adapter

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
 class TextSizeAdapter(private val context: Context, private val numbers: Array<Int>) : BaseAdapter() {
 
@@ -14,7 +15,9 @@ class TextSizeAdapter(private val context: Context, private val numbers: Array<I
     override fun getItemId(position: Int) = numbers[position].toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = numbers[position].toString()
+        return textView
     }
 
 }
